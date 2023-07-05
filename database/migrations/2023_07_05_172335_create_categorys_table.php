3ce1a53da68135866b47916ad0000e029292e5ee
luctk <luctkph26068@fpt.edu.vn>
2023-07-06 01:47:54 +0700

    tạo layout

---
+++ database/migrations/2023_07_05_172335_create_categorys_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('parent_id');
+            $table->timestamps();
+        });
+        Schema::create('category_product', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(\App\Models\Category::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(\App\Models\Product::class)->constrained()->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('category');
+    }
+};
